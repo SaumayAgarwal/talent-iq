8ea5df86549b85cc8c8f1df8e6b0039cf1000c6d
SaumayAgarwal <saumayagarwal6397@gmail.com>
2026-03-18 21:15:35 +0530

    setting up codebase

---
+++ backend/src/server.js
@@ -0,0 +1,27 @@
+// console.log("Hello, server is messaging.");
+import express from "express"
+import path from "path"
+import {ENV} from "./lib/env.js"
+
+const app = express();
+
+const __dirname = path.resolve();
+
+app.get("/health" , (req, res) => {
+    res.status(200).json({ message: "Hello, everybody!"});
+});
+
+app.get("/books" , (req, res) => {
+    res.status(200).json({ message: "This is our books."});
+});
+
+// make jjour app ready for deployment
+if(ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
+
+app.listen(ENV.PORT, () => console.log("Server is running on port: " + ENV.PORT));
